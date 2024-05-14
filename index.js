8a18c29160338dd03f88573cac95018885eb8c9f
victormglez <victormgb99@gmail.com>
2024-05-14 17:34:14 -0600

    index.js DB setup

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "victor98A",
+  port: 5432,
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
